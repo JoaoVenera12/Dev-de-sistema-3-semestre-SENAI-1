a6cb37d43f7c8d9938f2fce3ca2a45f260d93caf
Joaovenera <joaootavio.v@gmail.com>
2024-10-15 21:46:45 -0300

    fim do dia 15/10/2024

--- Aula_08-10-2024/sum.js
+++ Aula_08-10-2024/sum.js
@@ -1,4 +1,8 @@
-function sum(a, b){
-    return a+b;
+function sum(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error("Os argumentos devem ser números.");
+    }
+    return a + b;
 }
+
 module.exports = sum;
