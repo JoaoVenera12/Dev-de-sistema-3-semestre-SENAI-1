2880d2d101df4001853c11abdec41715b3be1dc4
Joaovenera <joaootavio.v@gmail.com>
2024-10-17 19:40:19 -0300

    Inicio do dia 17/10/2024

--- Desenvolvimento-de-Sistemas/Aula_10-10-2024/atividade2.js
+++ Desenvolvimento-de-Sistemas/Aula_10-10-2024/atividade2.js
@@ -1,7 +1,8 @@
-function ordenarNumeros(array) {
+function atividade2(array) {
     return array.sort((a, b) => a - b);
 }
 
+// Exemplo de uso
 let numeros = [5, 1, 4, 2];
-let resultado = ordenarNumeros(numeros);
+let resultado = atividade2(numeros);
 console.log(resultado);
